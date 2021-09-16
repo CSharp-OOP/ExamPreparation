674057ce994bc0f844e2b76ad97b743fd83b1483
Remzi <remzitdf@abv.bg>
2021-09-16 11:52:56 +0300

    Add files via upload

---
+++ C-Sharp OOP Retake Exam - 18 April 2019/PlayersAndMonsters/PlayersAndMonsters/Models/BattleFields/BattleField.cs
@@ -0,0 +1,74 @@
+﻿using PlayersAndMonsters.Models.BattleFields.Contracts;
+using PlayersAndMonsters.Models.Players.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlayersAndMonsters.Models.BattleFields
+{
+    public class BattleField : IBattleField
+    {
+        public void Fight(IPlayer attackPlayer, IPlayer enemyPlayer)
+        {
+            if (attackPlayer.IsDead || enemyPlayer.IsDead)
+            {
+                throw new ArgumentException("Player is dead!");
+            }
+
+            ModifyBeginnerPlayer(attackPlayer);
+            ModifyBeginnerPlayer(enemyPlayer);
+
+            attackPlayer = BoostPlayerHealth(attackPlayer);
+            enemyPlayer = BoostPlayerHealth(enemyPlayer);
+
+            while (true)
+            {
+                var attacherAttackPoints = attackPlayer.CardRepository
+                    .Cards
+                    .Select(c => c.DamagePoints)
+                    .Sum();
+
+                enemyPlayer.TakeDamage(attacherAttackPoints);
+
+                if (enemyPlayer.IsDead)
+                {
+                    break;
+                }
+
+                var enemyPlayerAttackPoint = enemyPlayer.CardRepository
+                    .Cards
+                    .Select(c => c.DamagePoints)
+                    .Sum();
+
+                attackPlayer.TakeDamage(enemyPlayerAttackPoint);
+
+                if (attackPlayer.IsDead)
+                {
+                    break;
+                }
+            }
+        }
+
+        private IPlayer BoostPlayerHealth(IPlayer player)
+        {
+            player.Health += player.CardRepository
+                            .Cards.Select(c => c.HealthPoints)
+                            .Sum();
+
+            return player;
+        }
+
+        private static void ModifyBeginnerPlayer(IPlayer player)
+        {
+            if (player.GetType().Name == "Beginner")
+            {
+                player.Health += 40;
+                foreach (var card in player.CardRepository.Cards)
+                {
+                    card.DamagePoints += 30;
+                }
+            }
+        }
+    }
+}
